533b003b726ba1544a4790dd2854b8f93a5968f8
dewantara <dewantara.tirta@gmail.com>
2025-03-28 16:36:46 +0700

    fix(tailwind)

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ const config = {
     './pages/**/*.{js,jsx,ts,tsx}',
     './components/**/*.{js,jsx,ts,tsx}',
     './src/**/*.{js,jsx,ts,tsx}',
+    './app/chat/**/*.{js,jsx,ts,tsx}',
+    './app/chat/components/**/*.{js,jsx,ts,tsx}',
   ],
   prefix: "",
   theme: {
